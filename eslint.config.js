20a82be782793040efe549e3e07b4faa30c9ced4
Manasoko <manajiyah@gmail.com>
2025-10-21 01:37:46 +0100

    add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+// eslint.config.js
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+];
